a4273d4f3fb7ec5789987f76ca4aae3f79e7478c
lAbEl JD <83861199+DanielJimenezA@users.noreply.github.com>
2024-08-27 21:46:04 +0000

    Archivo Horas

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,15 @@
 // Placeholder to host a component
+// Hours.js
+import React from 'react';
+
+const Hours = () => {
+  return (
+    <div>
+      <h2>Horario de Apertura</h2>
+      <p><strong>Lunes a Viernes:</strong> 10 a.m. - 4 p.m.</p>
+      <p><strong>Sábados y Domingos:</strong> 9 a.m. - 8 p.m.</p>
+    </div>
+  );
+};
+
+export default Hours;
